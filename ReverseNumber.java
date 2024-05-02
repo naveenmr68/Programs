a7a6d39fcd3eb10c12bcc366b6debe7707805ed6
naveenmr68 <naveenmr68@gmail.com>
2024-05-02 17:02:07 +0530

    newly added

---
+++ ReverseNumber.java
@@ -0,0 +1,23 @@
+package Logics;
+import java.util.Scanner;
+
+public class ReverseNumber {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter a number :");
+		
+		int num =sc.nextInt();
+		
+		int rev =0;
+		
+		while(num!=0)
+		{
+			rev=rev*10+num%10;
+			num=num/10;								
+		}
+        System.out.println("The Reverse Number is :"+rev);
+	}
+
+}
